e0d8b9c77d47c31c6794f1e31a66b01356c8ffa5
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-03 14:48:05 +0600

    feat: handle validation error message formet and internal server error message

--- server/app/app.py
+++ server/app/app.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
-
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 app = FastAPI()
+
+# handle validation error 
+@app.exception_handler(RequestValidationError)
+async def validation_error_handler(request,exc):
+    errors = {}
+    for error in exc.errors():
+        errors[error['loc'][-1]] = error['msg']
+
+    return JSONResponse({'message':"Validation error!",'errors':errors},status_code=400)
+
+
+# handle internal server error
+@app.exception_handler(Exception)
+async def internal_server_error_handler(request, exc):
+    return JSONResponse({'message': 'Internal server error', 'detail': str(exc)}, status_code=500)
 
 @app.get("/")
 def index():
