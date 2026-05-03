0e1342f277aa5960c857726fa8a8040a0f49a9ed
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-03 15:27:33 +0600

    ref: change datetime setting and add number column and define the column is nullable or not

--- server/app/database/schema/user.py
+++ server/app/database/schema/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean, Enum, DateTime, func
+from sqlalchemy import String, Boolean, Enum
 import enum
-from datetime import datetime
+from datetime import datetime, timezone
 from ..db import Base
 
 class UserRole(enum.Enum):
@@ -12,21 +12,44 @@ class UserRole(enum.Enum):
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
-    name: Mapped[str] = mapped_column(String(100))
-    email: Mapped[str] = mapped_column(String(180), unique=True, index=True)
-    password: Mapped[str]
-    role: Mapped[UserRole] = mapped_column(Enum(UserRole))
-    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    email: Mapped[str] = mapped_column(
+        String(180),
+        unique=True,
+        nullable=False
+    )
+
+    mobile: Mapped[str] =  mapped_column(
+        String(15),
+        unique=True,
+        nullable=False
+    )
+
+    password: Mapped[str] = mapped_column(
+        nullable=False
+    )
+
+    role: Mapped[UserRole] = mapped_column(
+        Enum(UserRole),
+        nullable=False
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+        nullable=False
+    )
+
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
+        default=lambda: datetime.now(timezone.utc),
         nullable=False
     )
 
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        onupdate=func.now(),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
         nullable=False
     )
