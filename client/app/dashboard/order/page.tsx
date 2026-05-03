e30360f894941ca6c2f57163480ad8d112171da5
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-03 14:21:37 +0600

    add: category slider, product list and right side with cart and payment modal

--- client/app/dashboard/order/page.tsx
+++ client/app/dashboard/order/page.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import CategorySlider from "./_components/CategorySlider";
+import ProductList from "./_components/ProductList";
+import RightSide from "./_components/RightSide";
 
 const OrderPage = () => {
-  return <div>OrderPage</div>;
+  return (
+    <>
+      <div className="flex justify-between w-full">
+        <div className=" overflow-hidden pt-10 lg:pr-6">
+          <CategorySlider />
+          <ProductList />
+        </div>
+
+        {/* right side */}
+        <RightSide />
+      </div>
+    </>
+  );
 };
 
 export default OrderPage;
