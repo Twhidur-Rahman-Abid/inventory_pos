a4c17c95f573d6777721f33471dc86d34183bd4d
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-05 16:25:49 +0600

    ref: show toltip for responsive

--- client/app/_components/dashboard/SideLInk.tsx
+++ client/app/_components/dashboard/SideLInk.tsx
@@ -16,7 +16,7 @@ export default function SideLink({ href = "", label = "", iconSrc = "" }) {
         href={hrefUrl}
         title={label}
         className={cn(
-          "group lg:w-60 p-2 md:p-4.5 flex gap-3 rounded-xl text-white",
+          "group lg:w-60 p-2 md:p-4.5 flex gap-3 rounded-xl text-white place-content-center",
           isActive ? "bg-primary " : "hover:bg-primary hover:text-white",
         )}
       >
@@ -25,15 +25,10 @@ export default function SideLink({ href = "", label = "", iconSrc = "" }) {
           alt={label}
           width={20}
           height={20}
-          className={cn(
-            "min-w-5 filter invert brightness-0",
-            // isActive
-            //  s ? "filter invert brightness-0"
-            //   : "group-hover:filter group-hover:invert group-hover:brightness-0",
-          )}
+          className={cn("min-w-5 filter invert brightness-0")}
         />
         <div className="hidden lg:flex w-full justify-between">
-          <p>{label}</p>
+          <p className="font-medium">{label}</p>
           {isActive && (
             <Image
               src="/icon/i-left-arrow.svg"
@@ -46,7 +41,7 @@ export default function SideLink({ href = "", label = "", iconSrc = "" }) {
       </Link>
 
       {/* ✅ Tooltip on small screens */}
-      <div className="lg:hidden absolute left-full top-1/2 -translate-y-1/2 ml-2 bg-gray-800 text-white text-sm px-2 py-1 rounded shadow opacity-0 group-hover:opacity-100 transition-opacity z-10 whitespace-nowrap">
+      <div className="lg:hidden absolute left-full top-1/2 -translate-y-1/2 ml-2 bg-gray-900 text-white text-sm px-2 py-1 rounded shadow opacity-0 group-hover:opacity-100 transition-opacity z-10 whitespace-nowrap">
         {label}
       </div>
     </div>
