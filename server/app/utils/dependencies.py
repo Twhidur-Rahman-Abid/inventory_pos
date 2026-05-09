a717e8d16b757956cf27cccc3637b14fb76da187
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-09 10:51:44 +0600

    feat: create authorize and role based access dependency

---
+++ server/app/utils/dependencies.py
@@ -0,0 +1,66 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from app.database.db import get_db
+from app.database.schema.user import User, UserRole
+from app.utils.auth import decode_token
+
+security = HTTPBearer()
+
+
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    db: AsyncSession = Depends(get_db)
+):
+
+    token = credentials.credentials
+
+    try:
+        payload = decode_token(token)
+
+        if payload.get("type") != "access":
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+
+        user_id = int(payload.get("sub"))
+
+        result = await db.execute(
+            select(User).where(User.id == user_id)
+        )
+
+        user = result.scalar_one_or_none()
+
+        if not user:
+            raise HTTPException(
+                status_code=401,
+                detail="User not found"
+            )
+
+        return user
+
+    except Exception:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
+
+
+def role_required(roles: list[UserRole]):
+
+    async def checker(
+        current_user: User = Depends(get_current_user)
+    ):
+
+        if current_user.role not in roles:
+            raise HTTPException(
+                status_code=403,
+                detail="Permission denied"
+            )
+
+        return current_user
+
+    return checker
