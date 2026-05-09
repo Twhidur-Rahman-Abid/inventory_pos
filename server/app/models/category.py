a3e5bf2216003c06311853f6148951530ebb6331
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-09 10:43:52 +0600

    feat: add category respone modal

--- server/app/models/category.py
+++ server/app/models/category.py
@@ -2,5 +2,11 @@ from pydantic import BaseModel
 from typing import Optional
 
 class CategoryCreate(BaseModel):
+    id:int
     name: str
     img: Optional[str] = None
+
+class CategoryResponse(BaseModel):
+    count:int
+    has_next:bool
+    data: list[CategoryCreate]
