d3caf1fd78340199aa139fb8a853b4876b8d9b5c
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-10 15:18:00 +0600

    ref: cash icon change

--- client/app/_constants/index.ts
+++ client/app/_constants/index.ts
@@ -3,7 +3,7 @@ const MONEY_SYMBOL = "৳";
 const MONEY_TITLE = "BDT";
 
 const PAYMENT_METHOD = [
-  { value: "cash", label: "Cash", img: "/cash.png" },
+  { value: "cash", label: "Cash", img: "/cash.svg" },
   { value: "Bkhas", label: "Bkash", img: "/BKash.svg" },
   { value: "nagad", label: "Nagad", img: "/Nagad.svg" },
   { value: "rocket", label: "Rocket", img: "/rocket.png" },
