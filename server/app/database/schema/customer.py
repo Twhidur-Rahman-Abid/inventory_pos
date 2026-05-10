116b4d0c47255bd1f65bf22081faa370fa9bd2cf
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-10 12:28:32 +0600

    ref: change string to varchar and mobile is not required

--- server/app/database/schema/customer.py
+++ server/app/database/schema/customer.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, DateTime, VARCHAR
 from datetime import datetime, timezone
 from ..db import Base
 
@@ -7,10 +7,10 @@ class Customer(Base):
     __tablename__ = "customers"
 
     id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
-    name: Mapped[str] = mapped_column(String(100),nullable=True)
-    email: Mapped[str] = mapped_column(String(180),nullable=False)
-    phone: Mapped[str] = mapped_column(String(15),nullable=True)
-    address: Mapped[str] = mapped_column(nullable=True)
+    name: Mapped[str] = mapped_column(VARCHAR(100),nullable=True)
+    email: Mapped[str] = mapped_column(VARCHAR(180),nullable=False)
+    phone: Mapped[str] = mapped_column(VARCHAR(15),nullable=True,index=True)
+    address: Mapped[str] = mapped_column(VARCHAR(255),nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), 
         default=lambda: datetime.now(timezone.utc), 
