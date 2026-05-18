bb1dabfcfca1ee043b832c411d805d142c9f7734
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-18 19:23:16 +0600

    feat: create delete func and add timestamp in file name

--- server/app/utils/utils.py
+++ server/app/utils/utils.py
@@ -1,11 +1,16 @@
+from datetime import datetime, timezone
+
 from fastapi import UploadFile
 from PIL import Image
 from pathlib import Path
 import os
 
+from app.config import get_config
+
 
 BASE_UPLOAD_DIR = Path("uploads")
 
+config = get_config()
 
 async def save_image(
     file: UploadFile,
@@ -21,7 +26,7 @@ async def save_image(
     upload_dir.mkdir(parents=True, exist_ok=True)
 
     # final filename
-    final_filename = f"{filename.replace(' ', '_').lower()}.webp"
+    final_filename = f"{filename.replace(' ', '_').lower()}_{datetime.now(timezone.utc).timestamp()}.webp"
 
     # full path
     file_path = upload_dir / final_filename
@@ -49,7 +54,37 @@ async def save_image(
         optimize=True
     )
 
-    return str(file_path).replace("\\", "/")
+    return config.site_link + '/' + str(file_path).replace("\\", "/")
+
+
+def delete_image_from_url(image_url: str | None) -> bool:
+   
+    if not image_url:
+        return False
+        
+    try:
+      
+        if "uploads/" in image_url:
+
+            relative_path_str = image_url.split("uploads/")[-1] 
+            
+
+            file_path = BASE_UPLOAD_DIR / relative_path_str
+
+         
+            if file_path.exists():
+                os.remove(file_path)
+
+                return True
+            else:
+                print(f"File not found on disk: {file_path}")
+        else:
+            print(f"Invalid image URL format: {image_url}")
+            
+    except Exception as e:
+        print(f"Error deleting image file from URL {image_url}: {str(e)}")
+        
+    return False
 
 def get_skip(page: int = 1, limit: int = 10) -> int:
     return (page - 1) * limit
