7bb53766f274e02f739f64be5a2ffc82a8b376a3
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-18 19:46:49 +0600

    fix: search functionality

--- client/app/_components/ui/Search.tsx
+++ client/app/_components/ui/Search.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { Suspense, useCallback } from "react";
-
+import React, { Suspense, useCallback, useMemo } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import Icon from "./Icon";
 import Input from "./Input";
@@ -8,29 +7,38 @@ import { debounce } from "../../_lib/utils";
 
 const SearchComp = () => {
   const searchParams = useSearchParams();
-  const search = searchParams.get("search") || "";
   const router = useRouter();
-  const handleSearch = (value: string) => {
-    const params = new URLSearchParams(searchParams.toString());
 
-    if (value) {
-      params.set("search", value);
-    } else {
-      params.delete("search");
-    }
+  const currentSearch = searchParams.get("search") || "";
 
-    params.set("page", "1");
+  const handleSearch = useCallback(
+    (value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
 
-    router.replace(`?${params.toString()}`);
-  };
-  // eslint-disable-next-line react-hooks/use-memo
-  const debouncedSearch = useCallback(debounce(handleSearch, 1000), []);
+      if (value) {
+        params.set("search", value);
+      } else {
+        params.delete("search");
+      }
+
+      params.set("page", "1");
+
+      router.replace(`?${params.toString()}`, { scroll: false });
+    },
+    [searchParams, router],
+  );
+
+  const debouncedSearch = useMemo(
+    () => debounce((value: string) => handleSearch(value), 1000),
+    [handleSearch],
+  );
+
   return (
     <Input
       placeholder="Search Here..."
       className="border-[1.5px] w-full md:max-w-75 grow"
       LeftIcon={<Icon src="/icon/i-search.svg" className="mr-2.5 md:mr-3.75" />}
-      defaultValue={search}
+      defaultValue={currentSearch}
       getInputValue={(value) => debouncedSearch(value)}
     />
   );
