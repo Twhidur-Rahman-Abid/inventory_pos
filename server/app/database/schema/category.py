6152524b348c3caf543efe96119f28791ed3bc0c
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-18 19:25:02 +0600

    feat: add cascade on delete

--- server/app/database/schema/category.py
+++ server/app/database/schema/category.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
+# product.py
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 from sqlalchemy import String,DateTime
 from datetime import datetime,timezone
 from ..db import Base
@@ -9,6 +10,13 @@ class Category(Base):
     id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
     name: Mapped[str] = mapped_column(String(100),nullable=False)
     img: Mapped[str] = mapped_column(nullable=True)
+
+    products: Mapped[list["Product"]] = relationship(
+        "Product", 
+        back_populates="category", 
+        cascade="all, delete-orphan",
+        passive_deletes=True
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), 
         default=lambda: datetime.now(timezone.utc), 
