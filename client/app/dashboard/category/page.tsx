96f7a9deac12bbe5b7387dac3a14b1dc8d2a9697
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-05-18 19:53:37 +0600

    feat: show category data,, search and delete category

--- client/app/dashboard/category/page.tsx
+++ client/app/dashboard/category/page.tsx
@@ -1,35 +1,101 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 
 import { HeaderType } from "@/app/_lib/CommonTypes";
 import {
   Button,
-  ExportTable,
   Icon,
   PageTopBar,
-  Pagination,
   Search,
-  ToggleSwitch,
+  DeleteItem,
 } from "@/app/_components";
 import Table, { Td } from "@/app/_components/ui/Table";
+import useFetchWAuth from "@/app/_hooks/useAuthFetch";
+import Loading from "@/app/_components/ui/Loading";
+import { ErrorMessage, NotFoundMessage } from "@/app/_components/ui/Alert";
 import Image from "next/image";
+import { CategoryType } from "@/app/_types/types";
 import CategoryModal from "./CategoryModal";
+import { useSearchParams } from "next/navigation";
 
 const tableHeaders: HeaderType[] = [
   { label: "Sl.", key: "serial" },
-  { label: "Name", key: "username" },
-  { label: "Image", key: "username" },
+  { label: "Name", key: "name" },
+  { label: "Image", key: "img" },
   { label: "Actions", key: "Actions", align: "center" },
 ];
 
-const CategoryPage = () => {
-  const [isCategoryOpen, setIsCategoryOpen] = useState(false);
+const BranchPage = () => {
+  const [categoryOpen, setCategoryOpen] = useState<
+    null | (Partial<CategoryType> & { open?: boolean })
+  >(null);
+  const { data, isLoading, status, error, fetcher } = useFetchWAuth<{
+    count: number;
+    data: CategoryType[];
+  }>({
+    endpoint: "/categories",
+  });
+
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search");
+
+  const searchFilter = (v: CategoryType) =>
+    search ? v.name.toLowerCase().includes(search.toLowerCase()) : true;
+
+  // decide what to render based fetch response
+  let content;
+  if (isLoading) content = <Loading />;
+  else if (!isLoading && status === "error")
+    content = <ErrorMessage message={error || "Failed to load data."} />;
+  else if (!isLoading && status === "success" && data?.count === 0)
+    content = <NotFoundMessage message="Category not found." />;
+  else
+    content = (
+      <>
+        <Table headers={tableHeaders}>
+          {data?.data
+            ?.filter(searchFilter)
+            ?.map((branch: CategoryType, index: number) => {
+              const { id, name, img } = branch;
+              return (
+                <tr key={id}>
+                  <Td>{index + 1}</Td>
+                  <Td>{name}</Td>
+                  <Td>
+                    {img ? (
+                      <Image src={img} width={24} height={24} alt={name} />
+                    ) : (
+                      "N/A"
+                    )}
+                  </Td>
+
+                  <Td>
+                    <div className="inline-flex gap-5 min-w-max items-center justify-center w-full">
+                      <Icon
+                        onClick={() => setCategoryOpen(branch)}
+                        src="/icon/i-edit-pen.svg"
+                        size={24}
+                      />
+
+                      <DeleteItem
+                        endpoint={`/categories/${id}`}
+                        fetcher={fetcher}
+                        title={`${name} Category`}
+                      />
+                    </div>
+                  </Td>
+                </tr>
+              );
+            })}
+        </Table>
+      </>
+    );
   return (
     <>
-      <PageTopBar title="Category" quantity={2}>
+      <PageTopBar title="Category" quantity={data?.count}>
         <Button
           className=" border-none px-3.5"
-          onClick={() => setIsCategoryOpen(true)}
+          onClick={() => setCategoryOpen({ open: true })}
         >
           ADD Category
           <Icon src="/icon/i-plus.svg" className="hidden md:inline-block" />
@@ -39,42 +105,18 @@ const CategoryPage = () => {
       <div className="card-wrapper space-y-6">
         <div className="flex gap-6 items-center justify-between flex-wrap">
           <Search />
-          <div className="flex gap-6 items-center">
-            <ExportTable headers={tableHeaders} tableData={[]} />
-          </div>
         </div>
-
-        <Table headers={tableHeaders}>
-          <tr>
-            <Td>1</Td>
-            <Td>Category 1</Td>
-            <Td>
-              <Image
-                width={60}
-                height={60}
-                src="/placeholder-img.svg"
-                alt="Category Image"
-                className="w-16 h-16 object-cover rounded"
-              />
-            </Td>
-
-            <Td>
-              <div className="w-full inline-flex justify-center items-center gap-5 min-w-max">
-                <Icon src="/icon/i-edit-pen.svg" size={24} />
-                <Icon src="/icon/i-eye-view.svg" size={24} />
-                <Icon src="/icon/i-delete.svg" size={24} />
-              </div>
-            </Td>
-          </tr>
-        </Table>
-
-        <Pagination totalPage={0} />
+        {content}
       </div>
-      {isCategoryOpen && (
-        <CategoryModal onClose={() => setIsCategoryOpen(false)} />
+      {categoryOpen && (
+        <CategoryModal
+          onClose={() => setCategoryOpen(null)}
+          fetcher={fetcher}
+          editable={categoryOpen}
+        />
       )}
     </>
   );
 };
 
-export default CategoryPage;
+export default BranchPage;
