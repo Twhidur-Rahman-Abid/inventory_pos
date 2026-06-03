15015e26fb4ac6c6ae5adcb57665fd3590bbe227
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-03 19:19:39 +0600

    feat: order response modal

--- server/app/models/order.py
+++ server/app/models/order.py
@@ -1,12 +1,25 @@
 from pydantic import BaseModel
-from typing import List
+from typing import Optional
+
+
+from app.database.schema.order import PaymentMethod, OrderStatus
 
 class OrderItemCreate(BaseModel):
     product_id: int
     qty: int
 
+
 class OrderCreate(BaseModel):
-    customer_id: int
+    customer_id: Optional[int] = None
     branch_id: int
-    items: List[OrderItemCreate]
-    payment_method: str
+    extra_discount: float = 0
+    delivery: float = 0
+    is_online: bool = False
+    note: Optional[str] = None
+    payment_method: PaymentMethod = PaymentMethod.CASH
+    items: list[OrderItemCreate]
+
+
+class OrderStatusUpdate(BaseModel):
+    status: OrderStatus
+
