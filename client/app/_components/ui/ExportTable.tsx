36da72be9a4d1106925571781bf3a7af24f71cdd
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-03 19:43:27 +0600

    fix: header type

--- client/app/_components/ui/ExportTable.tsx
+++ client/app/_components/ui/ExportTable.tsx
@@ -6,12 +6,14 @@ import Dropdown, { DropdownItem } from "./Dropdown";
 import Icon from "./Icon";
 import Arrow from "./Arrow";
 import Button from "./Button";
+import { exportTableData } from "@/app/_lib/exportTableData";
+import { HeaderType } from "@/app/_lib/CommonTypes";
 // import { exportTableData } from "@/lib/exportTableData";
 
 type ExportType = "pdf" | "excel" | "csv";
 
 type ExportProps = {
-  headers: Record<string, any>[];
+  headers: HeaderType[];
   tableData?: Record<string, any>[];
   filename?: string;
 };
@@ -22,14 +24,13 @@ const ExportTable = ({
   filename = "example_table",
 }: ExportProps) => {
   const handleDownload = (type: ExportType) => {
-    // exportTableData({
-    //   headers,
-    //   data: tableData,
-    //   excludeKeys: ["Status", "Actions"],
-    //   type,
-    //   filename,
-    // });
-    alert(`Exporting as ${type.toUpperCase()} is currently not implemented.`); // Placeholder alert
+    exportTableData({
+      headers,
+      data: tableData,
+      excludeKeys: ["Status", "Actions"],
+      type,
+      filename,
+    });
   };
 
   return (
