bb9e2ea8960b7aa6a1a32836e6d163d160098773
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-03 19:28:58 +0600

    feat: show cart data and billing summery

--- client/app/dashboard/order/_components/RightSide.tsx
+++ client/app/dashboard/order/_components/RightSide.tsx
@@ -6,6 +6,7 @@ import { Button, Icon, Input, Modal } from "@/app/_components";
 import Table, { Td } from "@/app/_components/ui/Table";
 import { cn } from "@/app/_lib/utils";
 import PaymentModal from "./PaymentModal";
+import { CartType, useCart } from "@/app/_context/productOrderCartContext";
 
 const tableHeaders = [
   { label: "Item" },
@@ -14,19 +15,24 @@ const tableHeaders = [
   { label: "Delete" },
 ];
 
-const carts = [
-  { id: 1, name: "Apple", quantity: 2, price: 50 },
-  { id: 2, name: "Banana", quantity: 1, price: 30 },
-];
-
 const RightSide = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isPaymentOpen, setIsPaymentOpen] = useState(false);
+  const [discount, setDiscount] = useState(0);
+  const [delivery, setDelivery] = useState(0);
 
-  const total = carts.reduce(
-    (acc, item) => acc + item.price * item.quantity,
-    0,
-  );
+  const { carts, clearCart } = useCart();
+
+  const subtotal = carts.reduce((acc, item) => acc + item.price * item.qty, 0);
+  const total = subtotal - discount + delivery;
+
+  const orderPayload = {
+    branch_id: 1,
+    extra_discount: discount,
+    delivery: delivery,
+    items: carts?.map((cart) => ({ product_id: cart.id, qty: cart.qty })),
+    total,
+  };
 
   return (
     <>
@@ -49,7 +55,7 @@ const RightSide = () => {
       {/* Sidebar */}
       <div
         className={cn(
-          `w-[80vw] absolute  z-20  top-4 transition-all ease-in-out duration-300 xl:static sm:min-w-fit sm:w-fit xl:block max-h-fit bg-white shadow-1 p-6 2xl:p-10 -translate-y-4  md:-translate-y-8 space-y-10`,
+          `w-[80vw] max-h-fit absolute  z-20  top-4 transition-all ease-in-out duration-300 xl:static sm:min-w-fit sm:w-fit xl:block  bg-white shadow-1 p-6 2xl:p-10 -translate-y-4  md:-translate-y-8 space-y-10`,
           {
             "right-0 top-4": isOpen,
             "-right-full": !isOpen,
@@ -66,12 +72,15 @@ const RightSide = () => {
           <div className="flex gap-5 items-center text-md font-semibold text-secondary">
             <p className="whitespace-nowrap">Total Item</p>
             <p className="flex justify-center items-center p-[5px] bg-[#F3FAF7] text-sm rounded-sm">
-              5
+              {carts?.length}
             </p>
           </div>
           <div className="flex gap-5 items-center">
             <Icon src="/icon/i-list-2.svg" size={28} />
-            <button className="flex gap-5 border-0  text-md font-semibold text-secondary  justify-center items-center p-1.25 bg-[#F3FAF7] text-sm rounded-sm cursor-pointer">
+            <button
+              onClick={() => clearCart()}
+              className="flex gap-5 border-0  text-md font-semibold text-secondary  justify-center items-center p-1.25 bg-[#F3FAF7] text-sm rounded-sm cursor-pointer"
+            >
               Clear
             </button>
           </div>
@@ -80,6 +89,7 @@ const RightSide = () => {
         <div className="max-h-60 w-full overflow-auto">
           {carts?.length > 0 ? (
             <Table
+              headerClassName="sticky top-0 left-0"
               className="p-3.5 min-w-fit overflow-hidden"
               headers={tableHeaders}
             >
@@ -97,13 +107,29 @@ const RightSide = () => {
           <Input placeholder="Phone" />
         </div>
         {/* Summary */}
-        <BillingSummary />
-        <Button onClick={() => setIsPaymentOpen(true)}>Pay {total} ৳</Button>
+        <BillingSummary
+          discount={discount}
+          setDiscount={setDiscount}
+          delivery={delivery}
+          setDelivery={setDelivery}
+          subtotal={subtotal || 0}
+          total={total || 0}
+        />
+        <Button
+          disabled={carts.length === 0}
+          onClick={() => setIsPaymentOpen(true)}
+        >
+          Pay {total} ৳
+        </Button>
       </div>
 
       {/* Payment Modal */}
       {isPaymentOpen && (
-        <PaymentModal onClose={() => setIsPaymentOpen(false)} />
+        <PaymentModal
+          onClose={() => setIsPaymentOpen(false)}
+          orderPayload={orderPayload}
+          onRightSideClose={() => setIsOpen(false)}
+        />
       )}
     </>
   );
@@ -111,48 +137,72 @@ const RightSide = () => {
 
 export default RightSide;
 
-function CartTableRow({ data = {} }) {
+function CartTableRow({ data }: { data: CartType }) {
+  const { removeCart, decrementCartQty, incrementCartQty } = useCart();
   return (
     <tr>
       {/* Product Name */}
-      <Td className="whitespace-nowrap p-3.5 truncate">Sample Product</Td>
+      <Td className="whitespace-nowrap p-3.5 truncate">{data.name}</Td>
 
       {/* Quantity */}
       <Td className="whitespace-nowrap p-3.5 text-center min-w-30">
         <div className="flex gap-4 justify-center items-center">
-          <Icon src="/icon/i-minus.svg" size={14} />
+          <Icon
+            src="/icon/i-minus.svg"
+            size={14}
+            onClick={() => decrementCartQty(data, 1)}
+          />
 
           <input
             type="number"
-            value={1}
+            value={data.qty}
             readOnly
             className="py-1 w-8 text-center rounded-sm text-black border-0 focus:outline-0 max-w-[30px] max-h-[30px] no-spinner overflow-auto text-sm bg-[#F3FAF7]"
           />
 
-          <Icon src="/icon/i-plus-border.svg" size={14} />
+          <Icon
+            src="/icon/i-plus-border.svg"
+            size={14}
+            onClick={() => incrementCartQty(data, 1)}
+          />
         </div>
       </Td>
 
       {/* Price */}
-      <Td className="whitespace-nowrap p-3.5">100.00</Td>
+      <Td className="whitespace-nowrap p-3.5">{data.price * data.qty}</Td>
 
       {/* Delete */}
       <Td className="mx-auto">
-        <Icon src="/icon/i-delete.svg" />
+        <Icon
+          src="/icon/i-delete.svg"
+          className="cursor-pointer"
+          onClick={() => removeCart(data.id)}
+        />
       </Td>
     </tr>
   );
 }
 
-function BillingSummary() {
+// ---- Billing Summary Component ----
+type BillingSummaryProps = {
+  discount: number;
+  setDiscount: (val: number) => void;
+  delivery: number;
+  setDelivery: (val: number) => void;
+  subtotal: number;
+  total: number;
+};
+
+function BillingSummary({
+  discount,
+  setDiscount,
+  delivery,
+  setDelivery,
+  subtotal = 0,
+  total = 0,
+}: BillingSummaryProps) {
   const [isDiscountOpen, setIsDiscountOpen] = useState(false);
   const [isDeliveryOpen, setIsDeliveryOpen] = useState(false);
-
-  const [discount, setDiscount] = useState(50);
-  const [delivery, setDelivery] = useState(30);
-
-  const subtotal = 500;
-  const total = subtotal - discount + delivery;
 
   return (
     <div className="pb-5">
