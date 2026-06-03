f6b9364279590e22445820e40eab26f1f99f4791
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-03 19:26:48 +0600

    feat: add loading page on dashboard page

---
+++ client/app/dashboard/loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Loading from "../_components/ui/Loading";
+
+const DashboardLoading = () => {
+  return (
+    <div className="flex-1 w-full py-6">
+      <Loading />
+    </div>
+  );
+};
+
+export default DashboardLoading;
