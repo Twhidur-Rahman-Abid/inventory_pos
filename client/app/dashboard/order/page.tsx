328430425db1f50cb5fb9f8738ffd759bb0b1986
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-03 19:30:20 +0600

    feat: add peoduct order cart contex provider

--- client/app/dashboard/order/page.tsx
+++ client/app/dashboard/order/page.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import CategorySlider from "./_components/CategorySlider";
 import ProductList from "./_components/ProductList";
 import RightSide from "./_components/RightSide";
+import { ProductOrderCartProvider } from "@/app/_context/productOrderCartContext";
 
 const OrderPage = () => {
   return (
-    <>
+    <ProductOrderCartProvider>
       <div className="flex justify-between w-full">
-        <div className=" overflow-hidden pt-10 lg:pr-6">
+        <div className=" overflow-hidden pt-10 lg:pr-6 flex-1">
           <CategorySlider />
           <ProductList />
         </div>
@@ -15,7 +16,7 @@ const OrderPage = () => {
         {/* right side */}
         <RightSide />
       </div>
-    </>
+    </ProductOrderCartProvider>
   );
 };
 
