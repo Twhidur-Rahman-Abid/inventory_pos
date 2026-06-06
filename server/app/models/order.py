ae2b214f648880c6ff8341b5ab8aa9716d6a0378
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-06 17:40:50 +0600

    feat: order item response model

--- server/app/models/order.py
+++ server/app/models/order.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 from typing import Optional
 
@@ -23,3 +25,51 @@ class OrderCreate(BaseModel):
 class OrderStatusUpdate(BaseModel):
     status: OrderStatus
 
+
+class ProductResponse(BaseModel):
+
+    id: int
+    sku_code: str
+    name: str
+  
+
+
+class OrderItemResponse(BaseModel):
+
+    id: int
+    qty: int
+    price: float
+    order_id: int
+    product_id: int
+    product: ProductResponse
+
+
+class CustomerResponse(BaseModel):
+    id: int
+    name: str
+    phone: str
+
+
+class OrderResponse(BaseModel):
+    id: int
+    customer_id: Optional[int] = None
+    branch_id: int
+
+    total: float
+    delivery: float
+    extra_discount: float
+
+    note: str
+    status: str
+    payment_method: str
+    is_online: bool
+
+    created_at: datetime
+    updated_at: datetime
+
+    customer: Optional[CustomerResponse] = None
+    items: list[OrderItemResponse]
+
+
+class OrderDetailsResponse(BaseModel):
+    data: OrderResponse
