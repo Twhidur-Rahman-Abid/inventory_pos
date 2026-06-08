c0c77dc052cf797be5a4fb3def7a753bfba5fb21
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-08 21:03:07 +0600

    feat: send product model

--- server/app/models/stock.py
+++ server/app/models/stock.py
@@ -1,6 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class StockCreate(BaseModel):
     product_id: int
     branch_id: int
     qty: int
+
+class SendStockSchema(BaseModel):
+    product_id: int
+    branch_id: int
+    quantity: int = Field(gt=0)
