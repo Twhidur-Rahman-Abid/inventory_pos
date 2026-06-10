5830698a8fa1d0e3ae251e103727aaf9fc8bcc13
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-10 12:30:28 +0600

    add server link

--- client/app/_constants/index.ts
+++ client/app/_constants/index.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://127.0.0.1:8000/api/v1";
+// const BASE_URL = "http://127.0.0.1:8000/api/v1";
+const BASE_URL = "https://niamah-shop.onrender.com/api/v1";
 
 // MONEY SYMBOL BDT
 const MONEY_SYMBOL = "৳";
