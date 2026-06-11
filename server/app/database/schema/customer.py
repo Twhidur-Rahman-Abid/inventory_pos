8e34f9a7736d41d1c260606f9452695ba110c788
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-06-11 12:09:36 +0600

    feat: only phone is req for user create

--- server/app/database/schema/customer.py
+++ server/app/database/schema/customer.py
@@ -9,8 +9,8 @@ class Customer(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True,autoincrement=True)
     name: Mapped[str] = mapped_column(VARCHAR(100),nullable=True)
-    email: Mapped[str] = mapped_column(VARCHAR(180),nullable=False)
-    phone: Mapped[str] = mapped_column(VARCHAR(15),nullable=True,index=True)
+    email: Mapped[str] = mapped_column(VARCHAR(180),nullable=True)
+    phone: Mapped[str] = mapped_column(VARCHAR(15),nullable=False,index=True)
     address: Mapped[str] = mapped_column(VARCHAR(255),nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), 
