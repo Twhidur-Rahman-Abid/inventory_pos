c6a4aec5815349519fb0ad65a322205c16f74028
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-08-31 11:54:47 +0600

    feat(client) add className props

--- client/app/_components/ui/InfoRow.tsx
+++ client/app/_components/ui/InfoRow.tsx
@@ -1,14 +1,17 @@
+import { cn } from "@/app/_lib/utils";
 import { ReactNode } from "react";
 
 const InfoRow = ({
   Left,
   Right,
+  className,
 }: {
   Left: ReactNode | string;
   Right: ReactNode | string;
+  className?: string;
 }) => {
   return (
-    <div className="grid grid-cols-2 place-items-center">
+    <div className={cn("grid grid-cols-2 place-items-center", className)}>
       <div className="w-full">{Left}</div>
       <div className="flex items-center w-full">
         <p className="max-w-fit mr-10">:</p>
