1102ffab066699e4f2c8e0d1a7bac16a87081fa6
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-08-31 11:28:03 +0600

    ref(server): change sellign_price > price and discounted_price > original_price

--- server/app/database/schema/order.py
+++ server/app/database/schema/order.py
@@ -85,8 +85,8 @@ class OrderItem(Base):
     )
 
     qty: Mapped[int] = mapped_column(nullable=False)
-    price: Mapped[float] = mapped_column(Numeric(10, 2),nullable=False)
-    discounted_price: Mapped[float] = mapped_column(Numeric(10, 2),nullable=True)
+    selling_price: Mapped[float] = mapped_column(Numeric(10, 2),nullable=False)
+    original_price: Mapped[float] = mapped_column(Numeric(10, 2),nullable=True)
     discount_type : Mapped[str] = mapped_column(String(60),nullable=True)
 
     order = relationship(
