a526408981e8b20db180f38df0dce2738cf8f082
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-09-06 21:38:39 +0600

    faet(client): use action fetch on client hook

---
+++ client/app/_actions/fetch_data.ts
@@ -0,0 +1,75 @@
+"use server";
+import { cacheLife, cacheTag, updateTag } from "next/cache";
+import { BASE_URL } from "../_constants";
+import { BranchType, CategoryType } from "../_types/types";
+
+// Fetch Response Interfaces
+export interface ApiResponse<T> {
+  success: boolean;
+  message: string;
+  data: T | null;
+}
+
+export interface FetchDataParams {
+  endpoint: string;
+}
+
+// Generic Fetch Function
+export const fetchData = async <T>({
+  endpoint,
+}: FetchDataParams): Promise<ApiResponse<T>> => {
+  try {
+    const res = await fetch(`${BASE_URL}${endpoint}`);
+    const data = await res.json();
+
+    if (!res.ok) {
+      console.error("error:", data);
+      return {
+        success: false,
+        message: data?.message || data?.details || "Something went wrong",
+        data: null,
+      };
+    } else {
+      return {
+        success: true,
+        message: "Fetched data success!",
+        data: data as T,
+      };
+    }
+  } catch (e) {
+    console.error(endpoint, ":", e);
+    return {
+      success: false,
+      message: "Client server error!",
+      data: null,
+    };
+  }
+};
+
+export const fetchCategory = async () => {
+  "use cache";
+  cacheLife("weeks");
+  cacheTag("category");
+  const res = await fetchData<{
+    count: number;
+    has_next: boolean;
+    data: CategoryType[];
+  }>({ endpoint: "/categories" });
+  if (!res) {
+    updateTag("category");
+  }
+  return res;
+};
+
+export const fetchBranch = async () => {
+  "use cache";
+  cacheLife("weeks");
+  cacheTag("branch");
+  const res = await fetchData<{ count: number; data: BranchType[] }>({
+    endpoint: "/branches",
+  });
+  if (!res) {
+    updateTag("branch");
+  }
+  return res;
+};
