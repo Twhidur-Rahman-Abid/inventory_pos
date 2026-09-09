a9609e5e725454c73f8c5762cba5aa9154fa9a5b
Towhidur-Rahman-Abid <abidrahman085@gmail.com>
2026-09-09 19:02:58 +0600

    feat(server): design web products response

--- server/app/models/web.py
+++ server/app/models/web.py
@@ -44,23 +44,7 @@ class BestProduct(BaseModel):
         return value
 
 
-class BestProduct(BaseModel):
-    id:int
-    sku_code: str
-    name: str
-    price: float
-    discount_percentage: Optional[float]
-    is_buy_one_get_one: bool = False
-    thumbnail: Optional[str] = None
-    quantity: int = 0
 
-    @field_validator("thumbnail", mode="before")
-    @classmethod
-    def format_img_url(cls, value: Optional[str]) -> Optional[str]:
-        if value and not value.startswith("http"):
-            relative_path = value.lstrip("/")
-            return f"{config.site_link}/{relative_path}"
-        return value
 
 class BestCategory(BaseModel):
     id:int
@@ -74,5 +58,40 @@ class BestCategory(BaseModel):
             relative_path = value.lstrip("/")
             return f"{config.site_link}/{relative_path}"
         return value
+
+
+
+# Single Product Item Schema
+class ProductListItem(BaseModel):
+    id: int
+    sku_code: str
+    name: str
+    price: float
+    thumbnail: Optional[str] = None
+    discount_percentage: float = 0.0
+    is_buy_one_get_one: bool = False
+    quantity: int
+    brand_id: Optional[int] = None
+    category_id: int
+    category_name: str
+
+    class Config:
+        from_attributes = True
+    @field_validator("thumbnail", mode="before")
+    @classmethod
+    def format_img_url(cls, value: Optional[str]) -> Optional[str]:
+        if value and not value.startswith("http"):
+            relative_path = value.lstrip("/")
+            return f"{config.site_link}/{relative_path}"
+        return value
+
+# Paginated Response Schema
+class ProductListResponse(BaseModel):
+    data: List[ProductListItem]
+    count: int = 0
+    has_next: bool = False
+
+
+
 
 
